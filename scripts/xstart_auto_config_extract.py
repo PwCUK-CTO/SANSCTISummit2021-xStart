e0b199c4875bd809b15e12936fe1f28c9c5454c6
BitsOfBinary <48534004+BitsOfBinary@users.noreply.github.com>
2021-01-20 10:54:05 +0000

    Update xstart_auto_config_extract.py

--- scripts/xstart_auto_config_extract.py
+++ scripts/xstart_auto_config_extract.py
@@ -13,6 +13,20 @@ import json
 Description: Python 3 script to decode resources of the xStart malware family, and extract configuration from its shellcode
 Author: @BitsOfBinary
 
+License:
+Copyright 2021 PwC UK
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
 
